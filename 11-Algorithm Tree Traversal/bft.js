2f62afbf2dbd2175af4aa1dde68b05404ab94063
Kanishk Jaiswal <umangkj12@gmail.com>
2025-04-02 12:37:31 +0530

    BFT and DFT-preOrder

---
+++ 11-Algorithm Tree Traversal/bft.js
@@ -0,0 +1,90 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.right = null;
+        this.left = null;
+    }
+}
+
+class BST {
+    constructor() {
+        this.root = null;
+    }
+
+    insert(value) {
+        const newNode = new Node(value);
+        if (!this.root) {
+            this.root = newNode;
+            return this;
+        } else {
+            let temp = this.root;
+            while (true) {
+                if (newNode.value === temp.value) return undefined;
+                if (newNode.value > temp.value) {
+                    if (!temp.right) {
+                        temp.right = newNode;
+                        return this;
+                    }
+                    temp = temp.right;
+                } else {
+                    if (!temp.left) {
+                        temp.left = newNode;
+                        return this;
+                    }
+                    temp = temp.left;
+                }
+            }
+
+        }
+    }
+    
+    searchAnNode(value) {
+        if (!this.root) return false
+        let temp = this.root;
+        while (temp) {
+            if (value > temp.value) {
+                temp = temp.right;
+            } else if (value < temp.value) {
+                temp = temp.left;
+            } else {
+                return true
+            }
+        }
+        return false
+    }
+
+    minValNode(currentNode) {
+        while (currentNode.left != null) {
+            currentNode = currentNode.left;
+        }
+        return currentNode
+    }
+    BFS() {
+        let currentNode = this.root;
+        let queue = [];
+        let res = [];
+        queue.push(currentNode);
+        while (queue.length) {
+            currentNode = queue.shift();
+            res.push(currentNode.value);
+            if (currentNode.left) queue.push(currentNode.left);
+            if (currentNode.right) queue.push(currentNode.right);
+        }
+        return res;
+    }
+}
+
+function Test() {
+    let myBST = new BST();
+    myBST.insert(47);
+    myBST.insert(21);
+    myBST.insert(76);
+    myBST.insert(18);
+    myBST.insert(27);
+    myBST.insert(76);
+    myBST.insert(52);
+    myBST.insert(82);
+
+    console.log(myBST.BFS()); // [47,21,76,18,27,52,82]
+}
+Test();
